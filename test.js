2b8b9c26863caa8a4691dd3f8d1338f30e90657c
TrungHieu <trunghieudw@gmail.com>
2020-11-19 15:13:08 +0700

    hoan thanh co ban reponsive o menu tuy nhien van con mot vai loi nho

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += " reponsive";
     } else {
         x.className = "topnav";
     }
-  }
+}
